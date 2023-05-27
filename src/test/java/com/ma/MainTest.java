b2965efd2785bb8ed50eb11cdc1e4c36dc266da5
ma <1600325372@qq.com>
2023-05-27 17:39:03 +0800

    maven&springboot基础配置

---
+++ src/test/java/com/ma/MainTest.java
@@ -0,0 +1,12 @@
+package com.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+}
